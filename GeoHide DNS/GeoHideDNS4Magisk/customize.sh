f3dd80cc1aa347c77e4e3db0ca4eb18c01f8075c
Andycar <Andycar@mail.ru>
2025-06-20 17:27:12 +0300

    deleted:    GeoHide DNS/GeoHideDNS4Magisk

--- GeoHide DNS/GeoHideDNS4Magisk/customize.sh
+++
@@ -1,5 +0,0 @@
-if [ -a /system/etc/resolv.conf ]; then
-	mkdir -p $TMPDIR/system/etc/
-	printf "nameserver 185.87.51.182\nnameserver 1.1.1.1" >> $TMPDIR/system/etc/resolv.conf
-	touch $TMPDIR/auto_mount
-fi
